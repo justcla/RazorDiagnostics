194defbd198ff3215e081cecb7ee9a490f3c4510
Alex Gavrilov <alexgav@microsoft.com>
2018-10-03 18:15:39 -0700

    Add project files.

---
+++ RazorDiagnostics/DiagnosticUtilities/InstallationStateUtilities.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace RazorDiagnostics.DiagnosticUtilities
+{
+    internal class InstallationStateUtilities
+    {
+        private static bool? _isRazorDependencyInstalled;
+
+        const string _webComponentGroupId = "Microsoft.VisualStudio.ComponentGroup.Web";
+
+        internal static bool IsRazorDependencyInstalled
+        {
+            get
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+
+                if (!_isRazorDependencyInstalled.HasValue)
+                {
+                    try
+                    {
+                        _isRazorDependencyInstalled = false;
+
+                        IVsSetupCompositionService setupCompositionService =
+                            ServiceProvider.GlobalProvider.GetService(typeof(SVsSetupCompositionService)) as IVsSetupCompositionService;
+
+                        Debug.Assert(setupCompositionService != null);
+                        if (setupCompositionService != null)
+                        {
+                            _isRazorDependencyInstalled = setupCompositionService.IsPackageInstalled(_webComponentGroupId);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Fail($"Exception while checking for {_webComponentGroupId} presence: \n{e.ToString()}");
+                    }
+
+                }
+
+                return _isRazorDependencyInstalled.Value;
+            }
+        }
+
+        internal static string InstalledWebEditorAssemblies => GetAssembliesInFolder(WebEditorFolder);
+
+        internal static string InstalledWebEditorRazorV4Assemblies => GetAssembliesInFolder(WebEditorRazor4Folder);
+
+        internal static string InstalledRazorExtensionAssemblies => GetAssembliesInFolder(RazorExtensionFolder);
+
+        internal static string WebEditorFolder => Path.Combine(IDEFolder, @"Extensions\Microsoft\Web Tools\Editors");
+
+        internal static string WebEditorRazor4Folder => Path.Combine(WebEditorFolder, @"Razor\v4.0");
+
+        internal static string RazorExtensionFolder => Path.Combine(IDEFolder, @"CommonExtensions\Microsoft\RazorLanguageServices");
+
+        internal static string IDEFolder => Path.GetDirectoryName(IDEPath);
+
+        internal static string IDEPath => Process.GetCurrentProcess().MainModule.FileName;
+
+        internal static string IDEInfo => GetAssemblyInfo(IDEPath);
+
+        internal static string GetAssembliesInFolder(string folderPath)
+        {
+            return "    " + string.Join("\r\n    ", Directory.GetFiles(folderPath, "*.dll", SearchOption.TopDirectoryOnly).Select(GetAssemblyInfo));
+        }
+
+        internal static string GetAssemblyInfo(string assemblyPath)
+        {
+            return Path.GetFileName(assemblyPath) + ", " + FileVersionInfo.GetVersionInfo(assemblyPath).ProductVersion;
+        }
+    }
+}
---
+++ RazorDiagnostics/OutputWindow.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorDiagnostics
+{
+    internal class OutputWindow
+    {
+        private static IVsOutputWindowPane _outputPane;
+        private static Guid _outputPaneGuid = new Guid("EDCCF70D-5A2C-48D4-8536-998F4FD1759C");
+
+        public static void OutputString(string toOutput, bool show = false)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            OutputPane.OutputString(toOutput);
+
+            if (show)
+            {
+                Show();
+            }
+        }
+
+        public static void Show()
+        {
+            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                IVsUIShell uiShell = await ServiceProvider.GetGlobalServiceAsync<SVsUIShell, IVsUIShell>();
+
+                IVsWindowFrame outputWindowFrame = null;
+                Guid outoutToolWindowGuid = new Guid("{ 0x34e76e81, 0xee4a, 0x11d0, {0xae, 0x2e, 0x00, 0xa0, 0xc9, 0x0f, 0xff, 0xc3 } }");
+                int hr = uiShell.FindToolWindow((uint)__VSFINDTOOLWIN.FTW_fForceCreate, ref outoutToolWindowGuid, out outputWindowFrame);
+
+                if (ErrorHandler.Succeeded(hr) && outputWindowFrame != null)
+                {
+                    hr = outputWindowFrame.ShowNoActivate();
+                    if (ErrorHandler.Succeeded(hr) && OutputPane != null)
+                    {
+                        OutputPane.Activate();
+                    }
+                }
+            });
+        }
+
+        public static IVsOutputWindowPane OutputPane
+        {
+            get
+            {
+                if (_outputPane == null)
+                {
+                    ThreadHelper.JoinableTaskFactory.Run(async () =>
+                    {
+                        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                        IVsOutputWindow outputWindow = await ServiceProvider.GetGlobalServiceAsync<SVsOutputWindow, IVsOutputWindow>();
+                        if (outputWindow != null)
+                        {
+                            if (ErrorHandler.Failed(outputWindow.GetPane(ref _outputPaneGuid, out _outputPane)) &&
+                                ErrorHandler.Succeeded(outputWindow.CreatePane(ref _outputPaneGuid, "Razor Diagnostics", 0, 0)))
+                            {
+                                if (ErrorHandler.Succeeded(outputWindow.GetPane(ref _outputPaneGuid, out _outputPane)))
+                                {
+                                    _outputPane.Activate();
+                                }
+                            }
+                        }
+                    });
+                }
+
+                return _outputPane;
+            }
+        }
+    }
+}
